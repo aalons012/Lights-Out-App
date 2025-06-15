19cb4a83584cddea3168acce658014c341c1a0fa
Andy Alonso <andyalonso281@gmail.com>
2025-06-15 19:47:17 -0400

    added XML and Kotlin coding, and deleted a xml file that isnt necessary

---
+++ app/src/main/java/edu/alonso/lights_out/LightsOutGame.kt
@@ -0,0 +1,46 @@
+package edu.alonso.lights_out
+
+import kotlin.random.Random // Imports the Random class from the kotlin.random package
+
+const val GRID_SIZE = 3     // Outputs the grid size for the game (3x3)
+
+class LightsOutGame {
+    private val lightsGrid = Array(GRID_SIZE) { BooleanArray(GRID_SIZE) {true} }
+
+    fun newGame(){
+        for (row in 0 until GRID_SIZE){
+            for (col in 0 until GRID_SIZE){
+                lightsGrid[row][col] = Random.nextBoolean()
+            }
+        }
+    }
+    fun isLightOn(row: Int, col: Int): Boolean{
+        return lightsGrid[row][col]
+    }
+    fun selectLight(row: Int, col: Int){
+        lightsGrid[row][col] = !lightsGrid[row][col]
+        if (row > 0){
+            lightsGrid[row - 1][col] = !lightsGrid[row - 1][col]
+        }
+        if (row < GRID_SIZE - 1){
+            lightsGrid[row + 1][col] = !lightsGrid[row + 1][col]
+        }
+        if (col > 0){
+            lightsGrid[row][col - 1] = !lightsGrid[row][col - 1]
+        }
+        if (col < GRID_SIZE - 1){
+            lightsGrid[row][col + 1] = !lightsGrid[row][col + 1]
+        }
+    }
+    val isGameOver: Boolean // Verify if its game over
+        get(){
+            for (row in 0 until GRID_SIZE){
+                for (col in 0 until GRID_SIZE){
+                    if (lightsGrid[row][col]){
+                        return false
+                    }
+                }
+            }
+            return true
+        }
+}
